58c4e1a206df785cbcd870822de5b88927070aff
LayRosCloud <65607542+LayRosCloud@users.noreply.github.com>
2023-06-12 21:30:50 +0700

    add icons

--- Assets/Skript/MenuSettings.cs
+++ Assets/Skript/MenuSettings.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class MenuSettings : MonoBehaviour
 {
+    [SerializeField] private AudioMixerGroup _audioMixerGroup;
+    [SerializeField] private GameObject _pauseObject;
     public void ClearSave()
     {
-
         PlayerPrefs.DeleteAll();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void OnMusic(bool enabled)
+    {
+        float value = enabled ? 0 : -80;
+        
+        _audioMixerGroup.audioMixer.SetFloat("MasterVolume", value);
+    }
+
+    public void Pause()
+    {
+        _pauseObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        _pauseObject.SetActive(false);
+        Time.timeScale = 1;
     }
 }
